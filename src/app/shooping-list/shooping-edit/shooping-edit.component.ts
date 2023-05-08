f08942a6f838f9a0c728b5b2f4756aabdae298ab
SakshiNayan <sakshi12princi@gmail.com>
2023-05-08 18:16:19 +0530

    reset the form

--- src/app/shooping-list/shooping-edit/shooping-edit.component.ts
+++ src/app/shooping-list/shooping-edit/shooping-edit.component.ts
@@ -32,14 +32,17 @@ export class ShoopingEditComponent implements OnInit, OnDestroy {
       )
   }
 
-  onAddItem(form: NgForm){
+  onSubmitItem(form: NgForm){
     const value = form.value;
     const newIngredient = new Ingredient(value.name , value.amount);
     if(this.editMode){
       this.slService.updateIngredient(this.editedItemIndex, newIngredient)
     }
     else{
-      this.slService.addIngredient(newIngredient);    }
+      this.slService.addIngredient(newIngredient);    
+    }
+    this.editMode = false;
+    form.reset(); // for reset the form after adding items
   }
 
   ngOnDestroy(): void {
